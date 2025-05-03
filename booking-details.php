9bd98db18aa8ce223fe716ad89c670e8bd94ca8e
Sohologo <tanushreesarkar.0301@gmail.com>
2025-05-04 01:35:46 +0530

    Add files via upload

--- booking-details.php
+++ booking-details.php
@@ -1,82 +1,59 @@
-<?php  
+<?php
 session_start();
 //error_reporting(0);
 include('includes/dbconnection.php');
-if (strlen($_SESSION['eahpaid']==0)) {
-  header('location:logout.php');
-  } else{
-
- if(isset($_POST['submit']))
-{
-    
-    $bookingnum = $_GET['bookingnum'];
-    $remark = $_POST['remark']; 
-    $status = $_POST['status'];
- $ambregno2=$_POST['ambregno'];
-
-if($ambregno2!=''):
-     $ambulanceregnum = $_POST['ambregno'];
-       else:
-   $ambulanceregnum = $_POST['ambulanceregnum'];
-       endif; 
-//        echo $ambulanceregnum;
-// exit();
-    $updateQuery1 = mysqli_query($con, "UPDATE tblambulance SET status='$status' WHERE AmbRegNum='$ambulanceregnum'");
-   $updateQuery2= mysqli_query($con, "UPDATE tblambulancehiring SET Status='$status', Remark='$remark', AmbulanceRegNo='$ambulanceregnum' WHERE BookingNumber='$bookingnum'");
-   $insertQuery = mysqli_query($con, "INSERT INTO tbltrackinghistory(BookingNumber, AmbulanceRegNum, Remark, Status) VALUES ('$bookingnum', '$ambulanceregnum', '$remark', '$status')");
-
-    if ($updateQuery1 && $updateQuery2 && $insertQuery) {
-        echo '<script>alert("Request Status Has been updated.")</script>';
-        echo "<script type='text/javascript'> document.location ='all-amublance-request.php'; </script>";
-    } else {
-        echo '<script>alert("Something Went Wrong. Please try again.")</script>';
-    }
-}
-
-
 ?>
-
-
 <!DOCTYPE html>
-<head>
-<title>EAHP || Booking Details</title>
+<html lang="en">
 
-<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-<!-- bootstrap-css -->
-<link rel="stylesheet" href="css/bootstrap.min.css" >
-<!-- //bootstrap-css -->
-<!-- Custom CSS -->
-<link href="css/style.css" rel='stylesheet' type='text/css' />
-<link href="css/style-responsive.css" rel="stylesheet"/>
-<!-- font CSS -->
-<link href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
-<!-- font-awesome icons -->
-<link rel="stylesheet" href="css/font.css" type="text/css"/>
-<link href="css/font-awesome.css" rel="stylesheet"> 
-<!-- //font-awesome icons -->
-<script src="js/jquery2.0.3.min.js"></script>
+<head>
+ 
+
+  <title>Emergancy Ambulance Hiring Portal || Tracking Page</title>
+
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
 </head>
+
 <body>
-<section id="container">
-<!--header start-->
-<?php include_once('includes/header.php');?>
-<!--header end-->
-<!--sidebar start-->
-<?php include_once('includes/sidebar.php');?>
-<!--sidebar end-->
-<!--main content start-->
-<section id="main-content">
-  <section class="wrapper">
-    <div class="table-agile-info">
- <div class="panel panel-default">
-    
-    <div class="panel-heading">
- Booking Details for an Ambulance
-    </div>
-    <div>
-<?php
+
+  <?php include_once('includes/header.php');?>
+
+  <main id="main">
+
+    <!-- ======= Breadcrumbs Section ======= -->
+    <section class="breadcrumbs">
+      <div class="container">
+
+        <div class="d-flex justify-content-between align-items-center">
+          <h2>Ambulance Tracking</h2>
+          <ol>
+            <li><a href="index.php" href="#hero">Home</a></li>
+            <li>Ambulance Tracking</li>
+          </ol>
+        </div>
+
+      </div>
+    </section><!-- End Breadcrumbs Section -->
+
+    <section class="inner-page">
+      <div class="container">
+       <?php
 $id = $_GET['id'];   
-$ret = mysqli_query($con, "SELECT * FROM tblambulancehiring WHERE ID = '$id'");
+$ret = mysqli_query($con, "SELECT tblambulancehiring.*,tblambulance.AmbRegNum, tblambulance.DriverName,tblambulance.DriverContactNumber FROM tblambulancehiring 
+    left join tblambulance on tblambulance.AmbRegNum=tblambulancehiring.AmbulanceRegNo  WHERE tblambulancehiring.ID = '$id'");
 
 
 while ($row = mysqli_fetch_array($ret)) {
@@ -155,7 +132,23 @@ while ($row = mysqli_fetch_array($ret)) {
         <td><?php echo $row['Status']; ?></td>
         <?php endif; ?>
     </tr>
+  
+    </tr>
     <?php endif; ?>
+     <?php if ($row['Status'] != ''){ ?>
+    <tr>     
+       <th>Driver Name</th>
+        <td><?php echo $row['DriverName']; ?></td>
+         <th>Driver Contact Number</th>
+        <td><?php echo $row['DriverContactNumber']; ?></td>
+      </tr>
+ <?php }else {?>
+  <tr>     
+       <th>Driver Name</th>
+        <td>Not Assigned Yet</td>
+         <th>Driver Contact Number</th>
+        <td>Not Assigned Yet</td>
+      </tr><?php }?>
 </table>
 
 <?php
@@ -190,17 +183,17 @@ while($row1=mysqli_fetch_array($query1))
 <td><?php echo htmlentities($row1['Remark']);?></td>
                  <td> <?php   $pstatus=$row1['Status'];  
                  if($pstatus==""){ ?>
-<span class="badge badge-info">New</span>
+<span>New</span>
  <?php } elseif($pstatus=="Assigned"){ ?>
-<span  class="badge badge-primary">Assigned</span>
+<span>Assigned</span>
  <?php } elseif($pstatus=="On the way"){ ?>
-<span class="badge badge-primary">On the Way</span>
+<span>On the Way</span>
  <?php } elseif($pstatus=="Pickup"){ ?>
-<span class="badge badge-success">Patient Pick</span>
+<span>Patient Pick</span>
  <?php } elseif($pstatus=="Reached"){ ?>
-<span class="badge badge-success">Patient Reached Hospital</span>
+<span>Patient Reached Hospital</span>
  <?php } elseif($pstatus=="Rejected"){ ?>
-<span class="badge badge-success">Rejected</span>
+<span>Rejected</span>
 
 <?php } ?>
 </td>
@@ -210,112 +203,26 @@ while($row1=mysqli_fetch_array($query1))
 </tr>
 <?php } ?>
 
-</table></div>
-<?php } ?>
-
- <?php if($pstatus=="" || $pstatus=="Assigned" || $pstatus=="On the way" || $pstatus=="Pickup"){ ?>
-<table border="1" class="table table-bordered mg-b-0">
-   
-    <tr><td colspan="6" style="font-size:18px;text-align: center;color: blue;">Administrator Work</td></tr>
-
-<form method="post" name="submit">
-
- <input type="hidden" name="ambregno" value="<?php echo $arnum;?>">
-  <tr>
-    <th>Status :</th>
-    <td> <select class=" form-control" id="status" name="status" type="text" required="true" value="">
-      <option value=""> Choose Status</option>
-   <?php if ($pstatus == ""): ?>
-    <option value="Assigned">Assigned</option>
-    <option value="Rejected">Rejected</option>
-<?php elseif ($pstatus == 'Assigned'): ?>
-    <option value="On the way">On the way</option>
-<?php elseif ($pstatus == 'On the way'): ?>
-    <option value="Pickup">Pick Patient</option>
-<?php elseif ($pstatus == 'Pickup'): ?>
-    <option value="Reached">Reached</option>
-<?php endif; ?>
-</select>
-    </td>
-  </tr>
-  <tr id="assign">
-    <th>Assign To :</th>
-    <td>
-   <select name="ambulanceregnum" id="ambulanceregnum" class="form-control wd-450" >
-
-<option value="">Select</option>
-     <?php $query=mysqli_query($con,"select * from tblambulance where Status is null || Status='Reached'");
-              while($row3=mysqli_fetch_array($query))
-              {
-              ?>    
-              <option value="<?php echo $row3['AmbRegNum'];?>"><?php   $atype=$row3['AmbulanceType'];  
-                 if($atype=="1"){ ?>
-                  <p>Howrah Hospital</p>
-                <?php } elseif($atype=="2"){ ?>
-                  <p>NRS Medical College</p>
-                <?php } elseif($atype=="3"){ ?>
-                   <p>Calcutta Medical College</p>
-                   <?php } elseif($atype=="4"){ ?>
-                     <p>BR Singh Hospital</p>
-                     <?php } ?>(<?php echo $row3['AmbRegNum'];?>)</option>
-                  <?php } ?> 
-     
-   </select></td>
-  </tr>
-  <tr>
-    <th>Remark :</th>
-    <td><textarea class="form-control" required name="remark"></textarea></td>
-  </tr>
-
-
-  <tr align="center">
-    <td colspan="2"><button type="submit" name="submit" class="btn btn-primary">Update</button></td>
-  </tr>
-  </form>
-
-
-<?php }  ?>
-
 </table>
+<?php } ?>
+      </div>
+    </section>
 
+  </main><!-- End #main -->
 
- 
-            
-          
-    </div>
-  </div>
-</div>
-</section>
- <!-- footer -->
-     <?php include_once('includes/footer.php');?>  
-  <!-- / footer -->
-</section>
+<?php include_once('includes/footer.php');?>
 
-<!--main content end-->
-</section>
-<script src="js/bootstrap.js"></script>
-<script src="js/jquery.dcjqaccordion.2.7.js"></script>
-<script src="js/scripts.js"></script>
-<script src="js/jquery.slimscroll.js"></script>
-<script src="js/jquery.nicescroll.js"></script>
-<!--[if lte IE 8]><script language="javascript" type="text/javascript" src="js/flot-chart/excanvas.min.js"></script><![endif]-->
-<script src="js/jquery.scrollTo.js"></script>
-<script type="text/javascript">
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/aos/aos.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
 
-  $('#assign').hide();
-  $(document).ready(function(){
-  $('#status').change(function(){
-  if($('#status').val()=='Assigned')
-  {
-  $('#assign').show();
-  jQuery("#ambulanceregnum").prop('required',true);  
-  }
-  else{
-  $('#assign').hide();
-  }
-})}) 
-</script>
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
 </body>
-</html>
 
-<?php }  ?>
+</html>
